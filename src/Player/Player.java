78041a58f3080b9392d080edc08070400df9b6d8
PM1354 <16petrm16@gmail.com>
2026-05-01 11:43:00 +0200

    Player hotov :)

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,48 +1,86 @@
 package Player;
 
 public class Player {
-    private int hitboxesX;
-    private int hitboxesY;
-    private int hearts;
+    private final int hitboxesX;
+    private final int hitboxesY;
+    private final int maxhearts;
     private int score;
-
+    private final int speed;
+    private int x;
+    private int y;
+    private int hearts;
 
     public int getHitboxesX() {
         return hitboxesX;
-    }
-
-    public void setHitboxesX(int hitboxesX) {
-        this.hitboxesX = hitboxesX;
     }
 
     public int getHitboxesY() {
         return hitboxesY;
     }
 
-    public void setHitboxesY(int hitboxesY) {
-        this.hitboxesY = hitboxesY;
-    }
-
-    public int getHearts() {
-        return hearts;
-    }
-
-    public void setHearts(int hearts) {
-        this.hearts = hearts;
+    public int getMaxhearts() {
+        return maxhearts;
     }
 
     public int getScore() {
         return score;
     }
 
-    public void setScore(int score) {
-        this.score = score;
+    public int getSpeed() {
+        return speed;
     }
 
-    public Player(int hitboxesY, int hitboxesX) {
-        this.hitboxesY = hitboxesY;
-        this.hitboxesX = hitboxesX;
-        this.hearts = 3;
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getHearts() {
+        return hearts;
+    }
+
+    public Player(int height, int width) {
+        this.hitboxesY = 16;
+        this.hitboxesX = 80;
+        this.maxhearts = 5;
         this.score = 0;
+        this.hearts = 3;
+        this.speed =6;
+        this.x =width/2;
+        this.y= height-40;
+    }
+
+    public void addScore(int i){
+        score +=i;
+    }
+
+    public void addHears(int i){
+        if (hearts<maxhearts){
+            hearts +=i;
+        }
+    }
+
+    public void looseHeart(){
+        if (hearts>0){
+            hearts--;
+        }
+    }
+
+    public boolean isAlive(){
+        if(hearts==0){
+            return false;
+        }
+        return true;
+    }
+
+    public void moveRight(){
+        x = x+speed;
+    }
+
+    public void moveLeft(){
+        x = x-speed;
     }
 }
